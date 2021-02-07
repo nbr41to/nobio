6b9dc0c6c06076981ea100788cbe4b2edc70d44d
nbr41to <nbr.41to@gmail.com>
2021-02-07 21:40:23 +0900

    metaとgtagを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,21 @@ import { GlobalStyle } from 'src/styles/GlobalStyle'
 import '../styles/gou.css'
 import '../styles/burger-styles.css'
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <Layout>
         <Head>
-
+          <title>nobio</title>
+          <meta charSet="utf-8" />
+          <meta name='description' content='nobio | 小林信之の自己紹介ページ 数学 プログラミング 心理学 教育 フリーランス' />
+          <meta property="og:title" content="nobio" />
+          <meta property="og:description" content="小林信之の自己紹介ページ 数学 プログラミング 心理学 教育 フリーランス" />
+          <meta property="og:url" content="https://nbr41.com" />
+          <meta property="og:site_name" content="nobio" />
+          <meta property="og:locale" content="ja_JP" />
+          <meta name="twitter:site" content="@Knob_nbr41to" />
         </Head>
         <Component {...pageProps} />
       </Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -32,7 +33,23 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <meta charSet="utf-8" />
+          {/* Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
