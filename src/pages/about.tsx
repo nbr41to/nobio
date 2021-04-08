28f5d5f997efbf416ebde0e9e2fcdc01776e0ee1
nbr41to <nbr.41to@gmail.com>
2021-04-08 20:12:21 +0900

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,10 +5,10 @@ import TestMdx from 'src/about-markdown/test.mdx'
 import { FadeInWrapper } from 'src/FadeInWrapper';
 export default function Home() {
   return (
-    <TopPage>
+    <AboutPage>
       <Heading id='###'>about me.</Heading>
-      <p>過去の経験から見えてきた自分の信念や大切にしていることをここに記録</p>
       <FadeInWrapper>
+        <p className='about-description'>過去の経験から見えてきた自分の性質や特性に加えて,信念や大切にしていることをここに記録していきます.</p>
         <Accordion label='Accordionです' labelColor='#fff' bgColor='#3549fc'>
           <p>中身を書いていくぅ！！</p>
         </Accordion>
@@ -34,11 +34,14 @@ export default function Home() {
           <p>中身を書いていくぅ！！</p>
         </Accordion>
       </FadeInWrapper>
-    </TopPage>
+    </AboutPage>
   )
 }
 
-const TopPage = styled.div`
+const AboutPage = styled.div`
   max-width: 1080px;
   margin: 0 auto;
+  .about-description {
+    margin: 8px 8px 20px;
+  }
 `
